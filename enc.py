95f3d7fbe3783cc3a5b30fec44fd379bd41652c2
STEIN <118396421+stein-exe@users.noreply.github.com>
2026-07-20 18:34:23 +0530

    Update enc.py

--- enc.py
+++ enc.py
@@ -9,14 +9,14 @@ __GROUP_CHAT__ = "@keped"
 import sys, urllib.request
 
 URLS = {
-    (3, 13): "https://raw.githubusercontent.com/stein-exe/hermit/refs/heads/main/files/3.13.py",
+    (3, 13): "https://raw.githubusercontent.com/stein-exe/hermit/refs/heads/main/files/3.14.py",
 }
 
 v = sys.version_info[:2]
 url = URLS.get(v)
 
 if not url:
-    print(f"Unsupported Python version: {v[0]}.{v[1]}. Use Python 3.13 only")
+    print(f"Unsupported Python version: {v[0]}.{v[1]}. Use Python 3.14 only")
     sys.exit(1)
 
 exec(urllib.request.urlopen(url).read().decode(), {'__name__': '__main__', '__file__': 'enc.py'})
