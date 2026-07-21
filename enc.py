2f7af7ef47686574ca5e6135461d4b6cda438459
STEIN <118396421+stein-exe@users.noreply.github.com>
2026-07-21 09:17:30 +0530

    Update enc.py

--- enc.py
+++ enc.py
@@ -1,26 +1,37 @@
+# ════════════════════
+__ENC_AUTHOR__ = "STEIN"
+__TELEGRAM__ = "@rejerk"
+__GROUP_CHAT__ = "@keped"
+# ════════════════════
+
+
 import sys
 import urllib.request
+from pathlib import Path
 
-major, minor = sys.version_info[:2]
+v = sys.version_info[:2]
+version = f"{v[0]}.{v[1]}"
 
-url = (
-    f"https://raw.githubusercontent.com/stein-exe/hermit/"
-    f"refs/heads/main/files/{major}.{minor}.py"
-)
+files_dir = Path("files")
+files_dir.mkdir(exist_ok=True)
 
-try:
-    code = urllib.request.urlopen(url).read().decode()
-except Exception:
-    print(
-        f"Unsupported Python version: {major}.{minor} "
-        f"(no matching file found)"
-    )
-    sys.exit(1)
+local_file = files_dir / f"{version}.py"
+
+if not local_file.exists():
+    url = f"https://raw.githubusercontent.com/stein-exe/hermit/refs/heads/main/files/{version}.py"
+
+    try:
+        local_file.write_bytes(
+            urllib.request.urlopen(url).read()
+        )
+    except Exception:
+        print(f"Unsupported Python version: {version}")
+        sys.exit(1)
 
 exec(
-    code,
+    local_file.read_text(encoding="utf-8"),
     {
         "__name__": "__main__",
-        "__file__": "enc.py",
-    },
+        "__file__": str(local_file)
+    }
 )
